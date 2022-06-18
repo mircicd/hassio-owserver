effbf9a3c2f36667b195db3396875fae9e6b310c
Dejan Mircic <dejan@codeto.ch>
2022-06-18 10:30:14 +0200

    Add support to configure `server` field of owconfig freely.

--- rootfs/etc/cont-init.d/owserver.sh
+++ rootfs/etc/cont-init.d/owserver.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
 
-declare device
-device=$(bashio::config 'device')
+declare server
+server=$(bashio::config 'server')
 
-if bashio::config.has_value 'device' ;then
-    bashio::log.info "Configuring ${device} device"
-    device=$(bashio::string.replace ${device} '/' '\/')
-    sed -i "s/%%device%%/${device}/g" /etc/owfs.conf
+if bashio::config.has_value 'server' ;then
+    bashio::log.info "Configuring server ${server}"
+    sed -i "s/%%server%%/${server}/g" /etc/owfs.conf
 else
     bashio::log.info "Configuring fake device"
-    sed -i "s/%%device%%/FAKE = DS18B20,DS2405/g" /etc/owfs.conf
+    sed -i "s/%%server%%/FAKE = DS18B20,DS2405/g" /etc/owfs.conf
 fi
